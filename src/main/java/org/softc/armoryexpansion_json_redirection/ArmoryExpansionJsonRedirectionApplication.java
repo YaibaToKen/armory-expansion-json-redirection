0ec96b92ac6b92414746c11df29384d869992c04
YaibaToKen <kurokyoudai@live.com>
2020-03-03 20:16:25 +0000

    Initial setup and configuration.

---
+++ src/main/java/org/softc/armoryexpansion_json_redirection/ArmoryExpansionJsonRedirectionApplication.java
@@ -0,0 +1,13 @@
+package org.softc.armoryexpansion_json_redirection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmoryExpansionJsonRedirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmoryExpansionJsonRedirectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/softc/armoryexpansion_json_redirection/ArmoryExpansionJsonRedirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softc.armoryexpansion_json_redirection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmoryExpansionJsonRedirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
